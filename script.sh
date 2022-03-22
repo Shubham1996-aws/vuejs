a07b5d87c6ce0413b83c7c87466c62fe971f547e
Shubham1996-aws <69294193+Shubham1996-aws@users.noreply.github.com>
2022-03-22 15:46:10 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,8 +2,5 @@
 npm install
 npm run build
 
-AWS Access Key ID [None]: AKIATBDDFP24ZG6LU6NI
-AWS Secret Access Key [None]: AKIATBDDFP24ZG6LU6NI
-Default region name [None]: ap-south-1
-Default output format [None]: json
+
 aws s3 sync ./dist s3://jenkinss3bucketadmin
